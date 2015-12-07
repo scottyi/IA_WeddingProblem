566393749aba6d67a63c36f0ed61c4085b78a7df
Zigabe jos <jos.zigabe@uclouvain.student.be>
2015-12-08 00:52:46 +0100

    initial solution

--- wedding.py
+++ wedding.py
@@ -27,36 +27,111 @@ import sys
 #################
 class Wedding(Problem):
 
-    # Defines the initial State
-	def __init__(self, init_state):
-    # Parse the file and put it in a matrix
-           file_init  = open(init_state, 'r')
-           affinities_matrix = []
-           i = -2
-           for line  in file_init:
-               if i == -2:
-                   nbr_guests = line
-                   i = i + 1
-               elif i == -1:
-                   nbr_tables = line
-                   i = i + 1
-               else :
-                     cols = line.split(" ")
-                     affinities_matrix.append([])
-                     for col in cols:
-                         affinities_matrix[i].append(int(col))
-               i+=1
+    def __init__(self, init):
 
-           self.initial = State(nbr_guests,nbr_tables,affinities_matrix)
-           file_init.close()
+        # Parse the input file
+        self.object = self.parse_file(init)
+        self.initial = self.initSolution(self.object)
 
     # Returns all differents states obtained by swapping 2 tables
-	def successor(self, state):
-         list_size = len(state.list)
-         for i in range(list_size):
-             for j in range(list_size):
-                 if i<j: #avoid identical swaps
-                     yield ((i,j),state.swap_tables(i,j))
+    def successor(self, state):
+        pass
+
+    def value(self, state):
+        pass
+
+    #read the file, create the list and code it for the state schema
+    def parse_file(self,path):
+
+        file_init  = open(path, 'r')
+        grid = []
+        i = -2
+
+        for line  in file_init:
+            if i == -2 :
+                    nbr_guests = line
+            elif i == -1 :
+                    nbr_tables = line
+            else :
+                #return all value in line, using sep as the delimiter string
+                cols = line.split(" ")
+                grid.append([])
+                for col in cols:
+                    if len(col)>0:
+                        grid[i].append(int(col))
+            i += 1
+
+        file_init.close()
+
+        return int(nbr_guests), int(nbr_tables), grid
+
+    def initSolution(self, object):
+
+        nbr_guests, nbr_tables, grid = object
+        #value of the initial solution
+        value = 0
+        #number of guests in each table
+        s = nbr_guests / nbr_tables
+
+        #set of tables
+        tables = []
+        t = 0
+
+        for i in range(nbr_guests-1):
+
+            #check if person i is already on a table
+            if self.isAvailable(i, tables) :
+                tables.append([])
+                #first unsigned person for a table
+                tables[t].append(int(i))
+
+                #fill tables[t]
+                value = self.fillTable(i, nbr_guests, s, tables[t], tables, grid, value)
+                #Go to the next table
+                t += 1
+        return value, tables
+
+
+    #Check person p is already on a table
+    def isAvailable(self, p, tables):
+        for table in tables :
+            for guest in table :
+                if guest == p :
+                    return False
+        return True
+
+    #Fill the table t
+    def fillTable(self, p, n, s, table, tables, grid, value):
+
+        #level of affinity [-5;+5]
+        aff_level = 5
+
+        #while the table[t] is not full
+        while(len(table) < s) :
+            for j in range(n):
+                #check if person j is already on a table
+                if self.isAvailable(j, tables):
+                    if grid[p][j] == aff_level:
+                        if len(table) < s :
+                            table.append(int(j))
+                            value += grid[p][j]
+            if aff_level == -5 :
+                aff_level = 5
+            else :
+                aff_level -= 1
+        #sort the table when is full
+        table.sort()
+        return value
+
+    #Get value of the solution
+    def getValue(self, tables):
+        value = 0
+        for table in tables :
+            value += sum(table)
+            print("Value")
+            print(value)
+        return value
+
 
 
 ###############
@@ -67,40 +142,13 @@ class Wedding(Problem):
 # A matrix is an attribute of the class State.
 class State:
 
-    def __init__(self, n, t, matrix ):
+    def __init__(self, n, t, m, tables, value):
+        self.guests = n
+        self.tables = t
+        self.affinities_matrix = m
+        self.tables = tables
+        self.value = value
 
-        self.guests = n # n guests
-        self.tables = t # t tables
-        self.affinities_matrix = matrix # affinities matrix
-        self.list = []
-
-    # Swap two tables
-    def swap_tables(self, table1, table2):
-        new_state = deepcopy(self)
-        tmp = new_state.list[table1]
-        new_state.list[table1] = new_state.list[table2]
-        new_state.list[table2] = tmp
-        return new_state
-
-    def __hash__(self):
-        s = ""
-        for x in range(0, len(self.list)):
-            s+=self.list[x]
-        return hash(s)
-
-
-    def __eq__(self, state):
-        return hash(self) == hash(state)
-
-
-    def __str__(self):
-        s = ""
-        for x in range(0, len(self.list)-1):
-            s += str(self.list[x]) + '->'
-        s += str(self.list[len(self.list)-1])
-        return s
-
-###########################################
 
 ################
 # Local Search #
@@ -111,48 +159,80 @@ class State:
 # If callback is not None, it must be a one-argument function that will be
 # called at each step with the current node
 def randomized_maxvalue(problem, limit=100, callback=None):
-        current = LSNode(problem, problem.initial, 0)
-        best = current
-        random.seed(42) # set the random "seed" to a static value
-        for step in range(limit):
-            if callback is not None:
-                callback(current)
-            best_list = []
+    current = LSNode(problem, problem.initial, 0)
+    best = current
 
-            for i in list(current.expand()):
-                best_list.append([i.value(),i])
+    for step in range(limit):
+        if callback is not None:
+            callback(current)
 
-            best_list = sorted(best_list, key=lambda b: b[0], reverse=True)
+        best_choices = []
+        min_value = 0
+        min_node = None
 
-            best_list = best_list[:5]
-            tmp = random.choice(best_list) # Chooses the next node randomly among the 5 best neighbors
+        for node in list(current.expand()):
+            if len(best_choices) < 5:
+                # If there is less than 5 node in the best_choices,
+                # we can add a node without removing another one from best_choices.
+                if min_node == None:
+                        # If this is the first node to add to best_choice
+                        # Initial step
+                        best_choices.append(node)
+                        min_node = node
+                        min_value = node.value()
+                else:
+                    best_choices.append(node)
+                    for best_node in best_choices:
+                        if best_node.value() < min_value:
+                            # we have to keep track of the current minimum node in case of remove
+                            min_value = best_node.value()
+                            min_node = best_node
+            else:
+                if node.value() > min_value:
+                    # Delete from best_choices the node containing the lowest value
+                    best_choices.remove(min_node)
+                    best_choices.append(node)
+                    min_value = node.value()
+                    min_node = node
+                    for best_node in best_choices:
+                        # Make sure that we keep track of the correct node
+                        # containing the minimum value
+                        if best_node.value() < min_value:
+                            min_value = best_node.value()
+                            min_node = best_node
 
-            current = tmp[1]
-            if tmp[0] > best.value():
-                best = current
-            #print(step, -current.value())
+        current = random.choice(best_choices)
 
-        return best
+        if current.value() > best.value():
+            best = current
+    return best
+
 
 # maxvalue chooses the best node (i.e., the node with maximum value) in the neighborhood,
 # even if it degrades the current  solution.
 # If callback is not None, it must be a one-argument function that will be
 # called at each step with the current node
-
 def maxvalue(problem, limit=100, callback=None):
-        current = LSNode(problem, problem.initial, 0)
-        best = current
-        for step in range(limit):
-            if callback is not None:
-                callback(current)
+    current = LSNode(problem, problem.initial, 0)
+    best = current
+    for step in range(limit):
+        if callback is not None:
+            callback(current)
 
-            for i in list(current.expand()):
-                if i.value() > best.value():
-                    best = i
-                    #best_step = step
-            #current = best
-            #print(step, -current.value())
-        return best
+        # max_value contains the current max value in the neighborhood
+        max_value = 0
+        # max_node contains the current node containing the current maximum value
+        max_node = current
+
+        for node in list(current.expand()):
+            if node.value() > max_value:
+                max_value = node.value()
+                max_node = node
+
+        current = max_node
+        if current.value() > best.value():
+            best = current
+    return best
 
 ####################
 # Launch the search#
@@ -162,9 +242,8 @@ if __name__ == '__main__':
 	wedding = Wedding(sys.argv[1])
 	print(wedding.initial)
 
-	node = randomized_maxvalue(wedding, 100)
+	#node = randomized_maxvalue(wedding, 100)
 	# node = maxvalue(wedding, 100)
-    # node = random_walk(wedding,100)
 
-	state = node.state
-	print(state)
+	#state = node.state
+	#print(state)
