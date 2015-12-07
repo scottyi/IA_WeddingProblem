89dd1c6974d351e6118edb3b94ca0621ba4904f3
Scott Ivinza <Scott@MacBook-Pro-de-Scott.local>
2015-12-07 01:33:36 +0100

    update du repository

--- studentSol.py
+++ studentSol.py
@@ -15,22 +15,30 @@ def get_clauses(merchant, level):
 		# 
 		# For example if you want to add the clauses equ1 or equ2 or ... or equN (i.e. a
 		# disjunction of all the equipment pieces the merchant proposes), you should write:
-		# 
-		# clauses.append(tuple(equ.index for equ in merchant.equipments))
-		#
 		clauses = []
-		#Provided
-		for equ1 in merchant.equipments:
-			for abi in equ1.provides:
-				clauses.append((-equ1.index,abi.index))
+		clauses.append(tuple(equ.index for equ in merchant.equipments))
 
-		#IsProvided
-		for abi in merchant.abilities:
-			clauses.append((-abi.index,)+tuple([provided.index for provided in abi.provided_by]))
+		for ability_name in level.ability_names:
+			for merchant_ability in merchant.abilities:
+				if ability_name == merchant_ability.name:
+					isProvided =[]
+					isProvided.append(-merchant_ability.index)
 
-		#Conflict
-		for equ2 in merchant.equipments:
-			clauses.append((-equ2.index,-(equ2.conflicts.index)))
+					clauses.append([merchant_ability.index])
+
+					for equipment in merchant_ability.provided_by:
+						isProvided.append(equipment.index)
+
+					clauses.append(isProvided)
+
+		for equipment in merchant.equipments:
+			conflicts = [-equipment.index]
+
+			for ability in equipment.provides:
+				clauses.append([-equipment.index,ability.index])
+			conflicts.append(-equipment.conflicts.index)
+			clauses.append(conflicts)
+
 
 		return clauses
 
@@ -42,18 +50,8 @@ def get_nb_vars(merchant, level):
 		# 
 		# For example, if your clauses contain all the equipments proposed by merchant and
 		# all the abilities provided by these equipment, you would have:
-		#nb_vars = len(merchant.abilities) + len(merchant.equipments)
-		diff_vars =[]
-		clauses = get_clauses(merchant,level) # Retourne toutes les clauses de la liste 'clauses'
-		for clause in clauses:
-			for element in clause :
-				isIn = False # isIn est mis initialement à False
-				for var in diff_vars:
-					if var == element:
-					    isIn = True
-				if not isIn:
-					diff_vars.append(element)
-		nb_vars = len(diff_vars)
+		nb_vars = len(merchant.abilities) + len(merchant.equipments)
+
 		return nb_vars
 
 
